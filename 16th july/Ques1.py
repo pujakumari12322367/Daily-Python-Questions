d1b1413558e37f7efd3cd0a13e672b62553e8f87
Avantika6190 <151778473+Avantika6190@users.noreply.github.com>
2026-07-16 11:16:00 +0530

    Add

---
+++ 16th july/Ques1.py
@@ -0,0 +1,8 @@
+# Write a Python program to test whether a number is within 100 of 1000 or 2000.
+
+num = int(input("Enter a number: "))
+
+if abs(1000 - num) <= 100 or abs(2000 - num) <= 100:
+    print("True")
+else:
+    print("False")
